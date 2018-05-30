eacd254c38fcd8731e0be4860036e033a41f73c1
Raimond Fowler <raifowler@gmail.com>
2018-05-29 20:29:28 -0700

    prepare-for-assessment.md

---
+++ prepare-for-assessment.md
@@ -0,0 +1,16 @@
+**Exercises**
+
+> In a new JS Bin, create the following functions. Submit a link to the JS Bin.
+
+* Write a function called `even` that accepts a single number as an argument and returns true if the number is even and false if the number is odd.
+
+* Write a function called `squared` that accepts a single number as an argument and returns the perfect square of the number. So `squared(5)` returns 25 and `squared(3)` returns 9.
+
+* Write a function called `food` that accepts no arguments and returns an array of your 3 favorite foods.
+
+* Write a function called `menu` that accepts no arguments and returns an object of your 3 favorite foods with the name of the foods as keys and the price of each item as the values.
+
+
+**Link to JS Bin**
+
+https://jsbin.com/buqojiy/edit?js,console
--- scratch.js
+++ scratch.js
@@ -1,24 +1,54 @@
+const even = function(num) {
+  if (num % 2 === 0 && num !== 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(even(2));
+
+
+const squared = function(num) {
+  return num * num;
+};
+
+console.log(squared(5));
+
+
+const food = function() {
+  return `Steak, pasta, and ice cream!`;
+};
+
+console.log(food());
+
+
+const menu = function() {
+  let menuItems = {
+    steak: 34.99,
+    pasta: 21.99,
+    iceCream: 12.99
+  }
+  return menuItems;
+};
+
+console.log(menu());
 
 
 
+// const integerSort = function(integers) {
+//   do {
+//     var swapped = false;
+//     integers.forEach((int, i) => {
+//       if(int > integers[i + 1]) {
+//         let temp = int;
+//         integers[i] = integers[i + 1];
+//         integers[i + 1] = temp;
+//         swapped = true;
+//       }
+//     })
+//   } while (swapped === true)
+//   return integers;
+// }
 
-
-
-
-
-const integerSort = function(integers) {
-  do {
-    var swapped = false;
-    integers.forEach((int, i) => {
-      if(int > integers[i + 1]) {
-        let temp = int;
-        integers[i] = integers[i + 1];
-        integers[i + 1] = temp;
-        swapped = true;
-      }
-    })
-  } while (swapped === true)
-  return integers;
-}
-
-console.log(integerSort([1,5,3,9,7,20,2]));
+// console.log(integerSort([1,5,3,9,7,20,2]));
